00da270f36bc713cf85bdaf9c974b0992314f8ac
mhcurylo <mhcurylo@gmail.com>
2017-02-14 22:13:40 +0000

    Evaluator binops

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -9,3 +9,28 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom f:args)) = apply f $ map eval args
+
+apply:: String -> [LispVal] -> LispVal
+apply f args = maybe (Bool False) ($ args) $ lookup f primitives 
+
+primitives:: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop:: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop f args = Number $ foldl1 f $ map unpackNum args
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in 
+                           if null parsed 
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
